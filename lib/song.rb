5057ef66c7d9bdb404575953a5b5ae76acdae69a
Natalia Castro <nat.alvz@gmail.com>
2018-08-02 18:36:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,13 @@ class Song
     artist.add_song(self)
   end
   
-    def genre=(genre)
+  def genre=(genre)
     @genre = genre
     genre.add_song(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.collect { |song| song.artist}
+    artists.uniq
   end
 end 
