a973336070d0c417f902d0a5bec29051d3c72d0e
Natalia Castro <nat.alvz@gmail.com>
2018-08-02 19:20:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,6 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@all = []
-  extend Concerns::Findable
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
@@ -42,5 +41,16 @@ class Song
     genre.add_song(self)
   end
 
+  def self.find_by_name(name)
+    self.all.detect {|item| item.name == name }
+  end
 
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song == nil
+      self.create(name)
+    else
+      return song
+    end
+  end
 end
