7583571627753968aa53aea7f9d4033867f428eb
Natalia Castro <nat.alvz@gmail.com>
2018-08-02 18:33:38 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,43 +1,43 @@
-class Genre 
-  
+class Genre
+
   attr_accessor :name, :songs
-  
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
     save
   end
-  
-  def self.all 
-    @@all 
-  end 
-  
+
+  def self.all
+    @@all
+  end
+
   def self.destroy_all
     @@all.clear
-  end 
-  
+  end
+
   def save
     @@all << self
-  end 
-  
-  def self.create(genre) 
+  end
+
+  def self.create(genre)
     self.new(genre)
-  end 
-  
+  end
+
     def add_song(song)
     if song.genre != self
       song.genre = self
     end
-    
+
     if !@songs.include?(song)
       @songs << song
-    end 
+    end
   end
 
   def artists
     artists = @songs.collect { |song| song.artist}
     artists.uniq
   endartists
-end 
+end
